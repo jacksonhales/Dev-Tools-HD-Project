a3af1e67a67ffef8fb24d105e20648cb1a0977d0
NICHOLAS HICKS <101467583@student.swin.edu.au>
2019-10-15 19:52:50 +1100

    added models, controller and default views for database and api

---
+++ WebApplication/Controllers/RolesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class RolesController : Controller
+    {
+        private ChallangeDataEntities db = new ChallangeDataEntities();
+
+        // GET: Roles
+        public ActionResult Index()
+        {
+            var roles = db.Roles.Include(r => r.Project).Include(r => r.User);
+            return View(roles.ToList());
+        }
+
+        // GET: Roles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Role role = db.Roles.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // GET: Roles/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProjectID = new SelectList(db.Projects, "ProjectID", "Descrption");
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "FirstName");
+            return View();
+        }
+
+        // POST: Roles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProjectID,UserID,Title,Description,Approved")] Role role)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Roles.Add(role);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProjectID = new SelectList(db.Projects, "ProjectID", "Descrption", role.ProjectID);
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "FirstName", role.UserID);
+            return View(role);
+        }
+
+        // GET: Roles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Role role = db.Roles.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProjectID = new SelectList(db.Projects, "ProjectID", "Descrption", role.ProjectID);
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "FirstName", role.UserID);
+            return View(role);
+        }
+
+        // POST: Roles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProjectID,UserID,Title,Description,Approved")] Role role)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(role).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProjectID = new SelectList(db.Projects, "ProjectID", "Descrption", role.ProjectID);
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "FirstName", role.UserID);
+            return View(role);
+        }
+
+        // GET: Roles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Role role = db.Roles.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // POST: Roles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Role role = db.Roles.Find(id);
+            db.Roles.Remove(role);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
