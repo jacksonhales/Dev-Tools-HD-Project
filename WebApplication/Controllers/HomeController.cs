695b8108309a5c9c641569dda85ad81ba1325901
DeadPixelz01 <deadpixelz01@gmail.com>
2019-10-29 05:54:44 -0500

    Added internal doc to the home page contoller

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -8,22 +8,26 @@ namespace WebApplication.Controllers
 {
     public class HomeController : Controller
     {
+        // index controller
         public ActionResult Index()
         {
+            // returns the user back to the index page
             return View();
         }
 
+        // about controller
         public ActionResult About()
         {
+            // returns the user to the site's about page
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
+        // contact controller
         public ActionResult Contact()
         {
+            // returns the user to the site's contact page
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
